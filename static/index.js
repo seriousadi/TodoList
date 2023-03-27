eda901e057e7bb26b042a856df8a5688fc3df3d2
Aditya Kumar <aaz22hsns@gmail.com>
2023-03-27 15:46:05 +0530

    improved some minor things

--- static/index.js
+++ static/index.js
@@ -1,3 +1,3 @@
 $(".addMore").click(function(){
-$("#listItems").append('<li><input name="listItem"class="list-group-item" placeholder="Items"> </li>')
+$("#listItems").append('<li><input name="listItem"  placeholder="Items"> </li>')
 })
